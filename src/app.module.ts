d3072966ac0b61d7b038a8f6570d79ae7807f720
kaan topal <kaant43@gmail.com>
2023-05-05 21:42:15 +0300

    add docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [],
+  imports: [
+    PrometheusModule.register({
+      path: '/metrics',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
